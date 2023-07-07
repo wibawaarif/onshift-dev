7e4cca2007d5714e78c7935cea98b50f78b85f60
Arif Wibawa <arifwb.work@gmail.com>
2023-07-07 22:56:37 +0700

    update weekly component

--- src/app/scheduler/page.jsx
+++ src/app/scheduler/page.jsx
@@ -15,7 +15,7 @@ const Scheduler = () => {
     <div className="h-screen w-screen bg-[#F7F7F7]">
       <div className="flex flex-col items-center pt-16 mb-6">
         <h1>Scheduler component</h1>
-        <Select className="mt-4" defaultValue="week" options={options} />
+        {/* <Select className="mt-4" defaultValue="week" options={options} /> */}
       </div>
       <SchedulerComponent />
     </div>
--- src/components/scheduler/page.jsx
+++ src/components/scheduler/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { Avatar } from "antd";
 
 const SchedulerComponent = () => {
-  const week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const week = ["Mon 7", "Tue 8", "Wed 9", "Thu 10", "Fri 11", "Sat 12", "Sun 13"];
 
   const employees = [
     {
@@ -15,7 +15,7 @@ const SchedulerComponent = () => {
       schedules: [
         {
           name: "Austin",
-          time: "1am to 5pm",
+          time: "1:00A - 5:00P",
           total: "7hrs",
         },
         {
@@ -41,7 +41,7 @@ const SchedulerComponent = () => {
         {
           name: "Isabelle",
           time: "12am to 10pm",
-          total: "57hrs",
+          total: "7hrs",
         },
         {
           name: "Etta",
@@ -95,7 +95,7 @@ const SchedulerComponent = () => {
     },
     {
       employeeDetail: {
-        name: "John Doe",
+        name: "Michael",
         totalHours: "8hr",
       },
       schedules: [
@@ -139,19 +139,53 @@ const SchedulerComponent = () => {
   ];
 
   return (
-    <div className="flex justify-center">
-      <div class="w-[75%] grid grid-cols-9">
-        <div class="col-span-2 bg-[#F1F1F1] flex items-center px-4">
-          <span className="text-slate-700">ALL EMPLOYEES</span>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-[1096px] h-[90px] bg-white border border-[1px] border-[#E5E5E3]">
+        {/* header table */}
+        <div className="p-6 flex h-full w-full justify-between items-center">
+          <div className="flex flex-col">
+            <p className="text-[20px]">Weekly Total</p>
+            <p className="text-[14px] text-[#7D7D80]">The data is displayed based on the specified filters</p>
+          </div>
+          <div className="h-[43px] w-[393px] flex justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-[20px]">6</span>
+              <span className="text-[#7D7D80] text-[10px]">SHIFTS</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="text-[20px]">40h</span>
+              <span className="text-[#7D7D80] text-[10px]">HOURS</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="text-[20px]">$800</span>
+              <span className="text-[#7D7D80] text-[10px]">WAGE</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="text-[20px]">$2000</span>
+              <span className="text-[#7D7D80] text-[10px]">SALES</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="text-[20px]">40%</span>
+              <span className="text-[#7D7D80] text-[10px]">LABOR %</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-[1096px] grid grid-cols-9">
+        <div class="col-span-2 bg-white flex items-center px-4 border border-[1px] border-[#E5E5E3]">
+          <Image className="mr-2" width={20} height={20} alt="sort"  src={'/static/svg/sort.svg'} />
+          <span className="text-[#191407]">First Name A-Z</span>
+          <Image width={24} height={24} alt="arrow-down"  src={'/static/svg/arrow-down.svg'} />
         </div>
 
         {week.map((x) => {
           return (
             <div
-              className="bg-[#DEDEDE] flex justify-center items-center h-[55px]"
+              className="bg-white flex justify-center items-center h-[40px] border border-[1px] border-[#E5E5E3]"
               key={x}
             >
               <span className="text-slate-700">{x}</span>
+              <Image className="ml-[8px]" alt='chart' width={16} height={16} src={'/static/svg/chart.svg'} />
             </div>
           );
         })}
@@ -159,58 +193,32 @@ const SchedulerComponent = () => {
         {employees.map((row, index) => (
           <>
             <div
-              className="flex justify-center items-center col-span-2 bg-white h-[87px]"
+              className="flex justify-center items-center col-span-2 border border-[1px] border-[#E5E5E3] bg-white h-[64px] w-[262px]"
               key={index}
             >
               <div className="flex justify-between p-4 h-full w-full">
                 <div className="flex items-center">
-                  <Avatar size={40} className="mr-2" />
+                  <Avatar size={32} className="mr-2" />
                   <div className="flex flex-col justify-between">
-                    <span>{row.employeeDetail.name}</span>
-                    <span>{row.employeeDetail.totalHours}</span>
+                    <span className="text-[14px]">{row.employeeDetail.name}</span>
                   </div>
                 </div>
-                <Image
-                  alt="action"
-                  width={20}
-                  height={20}
-                  src={"/static/svg/action.svg"}
-                />
               </div>
             </div>
 
+            {/* schedules */}
             {row.schedules.map((dataItem, dataIndex) => (
               <div
                 key={dataIndex}
-                className={`flex flex-col py-2 px-3 col-span-1 ${
+                className={`flex justify-center items-center col-span-1 ${
                   dataIndex === 0 ? "first-child" : ""
-                } bg-[#F1F1F1] h-[87px]`}
+                } bg-white border border-[1px] border-[#E5E5E3] h-full w-full`}
               >
-                <div className="flex flex-col h-full">
-                  <span className="text-sm">{dataItem.time}</span>
-                  <span className="text-xs">{dataItem.name}</span>
-                </div>
-                <div className="flex justify-between items-end h-full">
-                  <span className="text-xs">{dataItem.total}</span>
-                  <div className="flex">
-                    <Image
-                      alt="edit"
-                      width={16}
-                      height={16}
-                      src={"/static/svg/edit.svg"}
-                    />
-                    <Image
-                      alt="edit"
-                      width={16}
-                      height={16}
-                      src={"/static/svg/copy.svg"}
-                    />
-                    <Image
-                      alt="edit"
-                      width={16}
-                      height={16}
-                      src={"/static/svg/trash.svg"}
-                    />
+                <div className="bg-[#E5E5E3] h-[96%] w-[97%] rounded-sm p-2">
+                  <p className="text-[10px] font-semibold">{dataItem.time} · {dataItem.total}</p>
+                  <p className="mt-[1px] text-[10px]">LOCATION · POSITI...</p>
+                  <div className="w-[47px] h-[12px] bg-black rounded-xl flex justify-center items-center">
+                    <span className="text-white text-[8px]">PENDING</span>
                   </div>
                 </div>
               </div>
@@ -218,8 +226,8 @@ const SchedulerComponent = () => {
           </> 
         ))}
 
-        <div className="col-span-9 bg-[#F9F9F9] h-[60px]">
-          <div className="p-4 h-full w-full flex items-center bg-[#DEDEDE]">
+        <div className="col-span-9 bg-white h-[60px] border border-[1px] border-[#E5E5E3]">
+          <div className="p-4 h-full w-full flex items-center">
             <button>+ Add Employee</button>
           </div>
         </div>
