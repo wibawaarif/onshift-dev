1967b5e14d7ded6ee78445f818f8660c536db2b0
Arif Wibawa <105232319+wibawaarif@users.noreply.github.com>
2023-09-16 13:11:16 +0700

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -60,7 +60,7 @@ const handler = NextAuth({
     async signIn({ user, account, profile }) {
       if (account.provider === "google") {
         console.log('test');
-        const response = await fetch(`http://localhost:3000/api/register`, {
+        const response = await fetch(`/api/register`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
