67e38b0b31418e6cae7b6d81bd268446fa2a21cf
Arif Wibawa <105232319+wibawaarif@users.noreply.github.com>
2023-10-25 00:13:36 +0700

    change route login in landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -161,7 +161,7 @@ export default function Example() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="/signin" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
